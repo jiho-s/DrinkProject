7ddf56762c634a410fc4bebe602bc7c944ffc728
96glory <52440668+YoungKwang-Song@users.noreply.github.com>
2020-05-28 11:45:43 +0900

    initial

---
+++ src/main/java/com/b511/drink/DrinkApplication.java
@@ -0,0 +1,13 @@
+package com.b511.drink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b511/drink/DrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b511.drink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
