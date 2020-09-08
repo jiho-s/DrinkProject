60cd031c5084cb32ce7deda3bcd7b3271442e188
96glory <52440668+YoungKwang-Song@users.noreply.github.com>
2020-09-08 14:40:53 +0900

    머스태치로의 전환

--- src/main/java/com/b511/drink/IndexController.java
+++ src/main/java/com/b511/drink/IndexController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
     @GetMapping("/login")
     public String login(){
-        return "login.html";
+        return "login";
     }
+
 }
